4c19f33ea5fb784f2f2f23f11c9ce8f98f1f592d
suyeon77 <lsmn070@likelion.org>
2019-07-31 20:43:39 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lg\desktop\likelion\class lion\likelion_djaongo\django4\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
